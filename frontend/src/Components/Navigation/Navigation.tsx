1feb6e1775654c882cdee6da1e6ab686c9008036
klimkov.aa <klimkov.aa@phystech.edu>
2024-05-04 18:58:40 +0300

    обновлен внешний вид навигационного меню

--- frontend/src/Components/Navigation/Navigation.tsx
+++ frontend/src/Components/Navigation/Navigation.tsx
@@ -1,8 +1,16 @@
 import { Tab, Tabs } from "@mui/material"
 import NavigationButton from "../NavigationButton/NavigationButton";
 import { useState } from "react";
+import SsidChartRoundedIcon from '@mui/icons-material/SsidChartRounded';
+import PlayCircleFilledWhiteRoundedIcon from '@mui/icons-material/PlayCircleFilledWhiteRounded';
+import DisplaySettingsRoundedIcon from '@mui/icons-material/DisplaySettingsRounded';
 
 const Navigation = () => {
+    console.log(process.env.REACT_APP_NAVIGATION_TAB_COLOR);
+    const textColor = process.env.REACT_APP_NAVIGATION_TAB_COLOR === undefined
+        ? '#53B9EA'
+        : `#${process.env.REACT_APP_NAVIGATION_TAB_COLOR}`;
+
     const [tabState, setTabState] = useState(0);
     const handleTabs = (value: number) => {
         setTabState(value);
@@ -17,22 +25,28 @@ const Navigation = () => {
             value={tabState}
             >
             <Tab 
+                icon={<PlayCircleFilledWhiteRoundedIcon color="secondary"/>}
+                iconPosition="start"
                 resource="/launch_simulation" 
                 value={0}
                 label={
-                    <NavigationButton color="#53B9EA" label="Запуск симуляции"/>
+                    <NavigationButton color={textColor} label="Запуск нагрузки"/>
                 } />
             <Tab 
+                icon={<SsidChartRoundedIcon color="secondary" />}
+                iconPosition="start"
                 resource="/monitoring" 
                 value={1}
                 label={
-                    <NavigationButton color="#53B9EA" label="Мониторинг"/>
+                    <NavigationButton color={textColor} label="Мониторинг"/>
                 } />
             <Tab 
+                icon={<DisplaySettingsRoundedIcon color="secondary"/>}
+                iconPosition="start"
                 resource="/states" 
                 value={2}
                 label={
-                    <NavigationButton color="#53B9EA" label="Состояния"/>
+                    <NavigationButton color={textColor} label="Состояния"/>
                 } />
         </Tabs>
     )
